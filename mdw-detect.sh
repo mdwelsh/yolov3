2fa20ac7313e1e728fc8aa672df10231e5099cd4
Matt Welsh <mdw@octoml.ai>
2021-06-30 21:44:57 -0700

    Small change to test detection script.

--- mdw-detect.sh
+++ mdw-detect.sh
@@ -2,6 +2,10 @@
 
 # Use $HOME/venvs/yolov3/bin/activate
 
+#python ./detect.py \
+#  --source $HOME/datasets/mscoco/val2017 \
+#  --weights runs/train/exp5/weights/best.pt
+
 python ./detect.py \
-  --source $HOME/datasets/mscoco/val2017 \
+  --source $HOME/src/bricklens/python/detection_1000/images \
   --weights runs/train/exp5/weights/best.pt
