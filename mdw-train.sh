de7eda1c3ee6c1d59b2bb8a3a8c45f0695c60467
Matt Welsh <mdw@octoml.ai>
2021-11-24 22:18:24 -0800

    Training on bricklens_detection_2000_moreparts.

--- mdw-train.sh
+++ mdw-train.sh
@@ -25,11 +25,21 @@
 
 # Starting run 14 Nov 2021 with new 2000-image dataset.
 
+#python ./train.py \
+#  --project bricklens-yolov3 \
+#  --batch 4 \
+#  --epochs 1000 \
+#  --cfg models/yolov3.yaml \
+#  --data data/bricklens_detection_2000.yaml \
+#  --hyp data/hyp.bricklens.yaml
+
+# Starting run 24 Nov 2021 with new 2000-image-moreparts dataset.
+
 python ./train.py \
   --project bricklens-yolov3 \
   --batch 4 \
   --epochs 1000 \
   --cfg models/yolov3.yaml \
-  --data data/bricklens_detection_2000.yaml \
+  --data data/bricklens_detection_2000_moreparts.yaml \
   --hyp data/hyp.bricklens.yaml
 
