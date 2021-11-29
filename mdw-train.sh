40f6f175daa01e2bce95dd80db8957d019f4943f
Matt Welsh <mdw@octoml.ai>
2021-11-29 07:38:14 -0800

    Read classes directly from dataset.

--- mdw-train.sh
+++ mdw-train.sh
@@ -33,13 +33,23 @@
 #  --data data/bricklens_detection_2000.yaml \
 #  --hyp data/hyp.bricklens.yaml
 
+
 # Starting run 24 Nov 2021 with new 2000-image-moreparts dataset.
+
+#python ./train.py \
+#  --project bricklens-yolov3 \
+#  --batch 4 \
+#  --epochs 1000 \
+#  --cfg models/yolov3.yaml \
+#  --data data/bricklens_detection_2000_moreparts.yaml \
+#  --hyp data/hyp.bricklens.yaml
+
+# Starting run 29 Nov 2021 with bricklens_34parts_10colors_2000images dataset.
 
 python ./train.py \
   --project bricklens-yolov3 \
   --batch 4 \
-  --epochs 1000 \
+  --epochs 500 \
   --cfg models/yolov3.yaml \
-  --data data/bricklens_detection_2000_moreparts.yaml \
+  --data data/bricklens_34parts_10colors_2000images.yaml \
   --hyp data/hyp.bricklens.yaml
-
--- train.py
+++ train.py
@@ -64,9 +64,18 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
         check_dataset(data_dict)  # check
     train_path = data_dict['train']
     test_path = data_dict['val']
-    nc = 1 if opt.single_cls else int(data_dict['nc'])  # number of classes
-    names = ['item'] if opt.single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
-    assert len(names) == nc, '%g names found for nc=%g dataset in %s' % (len(names), nc, opt.data)  # check
+
+    classes_path = data_dict['classes']
+    names = []
+    with open(classes_path, "r") as classfile:
+        for line in classfile:
+            class_index, class_name = line.split()
+            names.append(class_name)
+    nc = len(names)
+
+    #nc = 1 if opt.single_cls else int(data_dict['nc'])  # number of classes
+    #names = ['item'] if opt.single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
+    #assert len(names) == nc, '%g names found for nc=%g dataset in %s' % (len(names), nc, opt.data)  # check
 
     # Model
     pretrained = weights.endswith('.pt')
