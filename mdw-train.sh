d996e4ca5ada0a6e7e4e5e82bafd8aef6bca8e67
Matt Welsh <mdw@octoml.ai>
2021-12-08 07:34:23 -0800

    10000-image training run.

--- mdw-train.sh
+++ mdw-train.sh
@@ -46,11 +46,21 @@
 
 # Starting run 29 Nov 2021 with bricklens_34parts_10colors_2000images dataset.
 
+#python ./train.py \
+#  --project bricklens-yolov3 \
+#  --batch 4 \
+#  --epochs 1000 \
+#  --cfg models/yolov3.yaml \
+#  --data data/bricklens_8parts_10colors_2000images.yaml \
+#  --hyp data/hyp.bricklens.yaml
+
+# Starting run 8 Dec2021 with bricklens_8parts_10colors_10000images dataset.
+# Note: Disabled augmentations in hyp.bricklens.yaml.
+
 python ./train.py \
   --project bricklens-yolov3 \
   --batch 4 \
-  --epochs 1000 \
+  --epochs 100 \
   --cfg models/yolov3.yaml \
-  --data data/bricklens_8parts_10colors_2000images.yaml \
+  --data data/bricklens_8parts_10colors_10000images.yaml \
   --hyp data/hyp.bricklens.yaml
-
