e163f863088bb24ecef83e05b5f54f8436c3b252
Matt Welsh <mdw@octoml.ai>
2022-03-17 16:04:46 -0700

    Small change to trace script.

--- mdw-trace.sh
+++ mdw-trace.sh
@@ -7,5 +7,5 @@ python ./test.py \
   --batch-size 4 \
   --img-size 512 \
   --data data/bricklens_8parts_nocolors_10000images.yaml \
-  --weights $HOME/models/bricklens/exp24/weights/best.pt
+  --weights bricklens-yolov3/exp24/weights/best.pt
 
